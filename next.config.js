56e1f97875e33eb830c1e95379204b19fc07f68f
Winkel Yin <winkel.yin@MTLMAC006.local>
2022-12-11 15:30:40 -0500

    setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['cdn.vox-cdn.com/'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
 import Header from '../components/Header';
 
-export default function Home() {
+function Resume() {
   return (
     <div>
       <Header />
-      <p>Props to Ally Zhang for being my frontend slave!</p>
     </div>
   );
 }
+
+export default Resume;
