e813ce8e7256bc2fe8f8add90aeaaba7a782502e
Winkel515 <winkel515@hotmail.com>
2022-12-11 22:15:52 -0500

    added art

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,16 +4,11 @@ import React from 'react';
 
 function Header() {
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+    <nav className="bg-gray-900 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <Link href="#" className="flex items-center">
-          <Image
-            src="/feelsbadman.png"
-            alt="Flowbite Logo"
-            width={50}
-            height={50}
-          />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          <Image src="/feelsbadman.png" alt="Logo" width={50} height={50} />
+          <span className="text-white self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             Winkel Yin
           </span>
         </Link>
@@ -40,11 +35,11 @@ function Header() {
           </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-gray-900 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
-                href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                href="/"
+                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-400 md:p-0 dark:text-white"
                 aria-current="page"
               >
                 Home
@@ -53,7 +48,7 @@ function Header() {
             <li>
               <Link
                 href="/resume"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Resume
               </Link>
@@ -61,7 +56,7 @@ function Header() {
             <li>
               <Link
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Blog
               </Link>
@@ -69,7 +64,7 @@ function Header() {
             <li>
               <Link
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Leetcode
               </Link>
@@ -77,7 +72,7 @@ function Header() {
             <li>
               <Link
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Contact
               </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
+import Image from 'next/image';
 import Header from '../components/Header';
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <p>Props to Ally Zhang for being my frontend slave!</p>
+      <p>Thank you Ally Zhang for being my frontend slave!</p>
+      <div className="flex content-center justify-center">
+        <Image src="/pepehands.png" alt="Logo" width={985} height={751} />
+      </div>
     </div>
   );
 }
